cf60b4566ea56aca2f6105c020fae7906dd2fe2d
Martin Ellegård <martin.ellegard@gmail.com>
2025-01-17 15:27:37 +0100

    Updated helix config and fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -19,10 +19,10 @@ set --export ASPNETCORE_URLS "http://*:8080"
 
 if test "$os" = Darwin
     # do things for macOS
-    set -U fish_user_paths /Users/martin/.local/bin /Users/martin/.local.bin /Users/martin/.nix-profile/bin /Applications /opt/homebrew/bin /opt/homebrew/sbin /opt/homebrew/opt/libpq/bin /Users/martin/go/bin /Users/martin/.omnisharp/bin /Users/martin/.cargo/bin
+    set -U fish_user_paths /Users/martin/.local/bin /Users/martin/.local.bin /Users/martin/.nix-profile/bin /Applications /opt/homebrew/bin /opt/homebrew/sbin /opt/homebrew/opt/libpq/bin /Users/martin/go/bin /Users/martin/.omnisharp/bin /Users/martin/.cargo/bin /Users/martin/.npm-global/bin
 else if test "$os" = Linux
     # do things for Linux
-    set -U fish_user_paths $HOME/.local/bin $HOME/.nix-profile/bin /nix/var/nix/profiles/default/bin $HOME/.cargo/bin $HOME/.cargo/bin $HOME/go/bin $fish_user_paths
+    set -U fish_user_paths $HOME/.local/bin $HOME/.nix-profile/bin /nix/var/nix/profiles/default/bin $HOME/.cargo/bin $HOME/.cargo/bin $HOME/go/bin $HOME/.npm-global/bin $fish_user_paths
     if test "$HYPRSTARTED" = STARTED
         if test "$RANDOMBGRUNNING" = RUNNING
             set -Ux RANDOMBGRUNNING RUNNING
